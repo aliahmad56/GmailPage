3db5cea7a360b93b5570123913d6c69bd93f4d69
aliahmad56 <aliahmad56b@gmail.com>
2022-11-23 01:09:55 +0500

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset ('Css/custom.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+      <section>
+      <div class="container">
+          <div class="row">
+              <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+              <center> <img src="{{asset ('images/googlelogo.png')}}" class="logoimage"></center>
+           <center><h4>One account. All of Google</h4></center>
+           <center><h6>Sign in to continue to Gmail</h6></center>
+                     </div>
+                        </div>
+                          </div>
+                             </section>  
+
+
+    <section>
+      <div class="container cardrow">
+</center><div class="row">
+              <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                             <div class="card" style="background-color: #F7F7F7; padding-bottom: 30px;">
+</br>
+                               <center> <img class="card-img-top usericon" src="{{asset ('images/usericon.jpg')}}" alt="Card image cap"></center>
+                                <div class="card-body">
+                                    
+                                <div class="form-group">
+                               
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></br>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></br>
+                                <button type="submit" class="btn btn-primary mb-2"style="width: 383px">Sign in</button></br>
+                            </div>                       
+                            
+                            <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox1">Stay singed in</label>
+                            </div>
+
+                            <div class="inline">
+                               <a href="#">Need Help ?</a>
+                                </div> 
+                                </div>
+                                </div>
+
+                        </div>
+                        </div></center>
+                          </div>
+                             </section> 
+
+                             <section class="main mt-3">
+                                 <div class="container">
+                                     <div class="row">
+                                         <div class="col-12 col-sm-12 col-lg-12 col-md-12 col-xl-12">
+
+                                         <center><a href="#">Create an account</a></center>
+
+                                        <center> <p class="mt-3">One Google Account for everything Google</p></center>
+                                       <div class="icon">
+                                        <i class="fa fa-facebook-official pr-3"></i>
+                                        <i class="fa fa-youtube-play pr-3" aria-hidden="true"></i>
+                                        <i class="fa fa-envelope-o pr-3" aria-hidden="true"></i>
+                                        <i class="fa fa-id-card pr-3" aria-hidden="true"></i>
+                                        <i class="fa fa-google-plus pr-3" aria-hidden="true"></i>
+                                        <i class="fa fa-map-marker pr-3" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+</section>
+   
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
